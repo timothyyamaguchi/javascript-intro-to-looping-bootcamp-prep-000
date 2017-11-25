f510194cd97518c966e572c38adb6f8ebe6bd489
Tim Yamaguchi <timothy.yamaguchi@comcast.net>
2017-11-25 02:25:12 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,10 @@
-for (let i = 0; i < 25; i++) {
-  console.log("I am " + i + " strange loop.")
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push('I am ${i} strange loops.')
+    }
+  }
+  return array
 }
